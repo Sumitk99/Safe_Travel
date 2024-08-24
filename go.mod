0caa22280ae3996dc735471bce3cfd91f3540dc5
Sumitk99 <sumit2k19kumar@gmail.com>
2024-08-24 12:17:13 +0530

    initial commit

---
+++ go.mod
@@ -0,0 +1,14 @@
+module call_api
+
+go 1.19
+
+require (
+	github.com/gorilla/mux v1.8.1
+	github.com/twilio/twilio-go v1.21.0
+)
+
+require (
+	github.com/golang/mock v1.6.0 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
+	github.com/stretchr/testify v1.9.0 // indirect
+)
---
+++ main.go
@@ -0,0 +1,120 @@
+package main
+
+import (
+	"fmt"
+	"github.com/gorilla/mux"
+	"github.com/twilio/twilio-go"
+	openapi "github.com/twilio/twilio-go/rest/api/v2010"
+	"log"
+	"net/http"
+	"os"
+	"time"
+)
+
+var fromPhoneNumber = os.Getenv("TWILIO_PHONE_NUMBER")
+var toPhoneNumber = os.Getenv("CLIENT_PHONE_NUMBER")
+var alertReceiver = os.Getenv("ALERT_RECEIVER")
+var accountSid = os.Getenv("TWILIO_ACCOUNT_SID")
+var authToken = os.Getenv("TWILIO_AUTH_TOKEN")
+
+type AlertMessage struct {
+	To      string `json:"to"`
+	From    string `json:"from"`
+	Message string `json:"message"`
+}
+
+func main() {
+	router := mux.NewRouter()
+	router.HandleFunc("/voice", voiceHandler).Methods("POST")
+	router.HandleFunc("/gather", gatherHandler).Methods("POST")
+
+	go startScheduler()
+
+	log.Println("Starting server on :8080")
+	log.Fatal(http.ListenAndServe(":8080", router))
+}
+
+func startScheduler() {
+	ticker := time.NewTicker(30 * time.Second)
+	defer ticker.Stop()
+
+	for {
+		<-ticker.C
+		log.Printf("Triggering call at %s\n", time.Now().Format(time.RFC3339))
+		triggerCall()
+	}
+}
+
+func triggerCall() {
+	client := twilio.NewRestClientWithParams(twilio.ClientParams{
+		Username: accountSid,
+		Password: authToken,
+	})
+
+	params := &openapi.CreateCallParams{}
+	params.SetTo(toPhoneNumber)
+	params.SetFrom(fromPhoneNumber)
+	params.SetUrl(os.Getenv("ONLINE_URL"))
+
+	resp, err := client.Api.CreateCall(params)
+	if err != nil {
+		log.Println("Error triggering call:", err)
+	} else {
+		log.Println("Call triggered:", *resp.Sid)
+	}
+}
+
+func voiceHandler(w http.ResponseWriter, r *http.Request) {
+	response := `
+        <Response>
+            <Gather input="dtmf" timeout="10" numDigits="1" action="/gather">
+                <Say>Please Enter the code</Say>
+            </Gather>
+        </Response>`
+	w.Header().Set("Content-Type", "application/xml")
+	w.Write([]byte(response))
+}
+
+func gatherHandler(w http.ResponseWriter, r *http.Request) {
+	err := r.ParseForm()
+	if err != nil {
+		log.Printf("Error parsing form data: %v", err)
+		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+		return
+	}
+	digits := r.FormValue("Digits")
+	log.Printf("User Response : %s", digits)
+
+	var response string
+	if digits == "1" {
+		response = "Thank you. Your code is correct."
+	} else {
+		response = fmt.Sprintf("Alert, Incorrect Code entered by %s.", toPhoneNumber)
+	}
+	sendAlert(response)
+}
+
+func sendAlert(CodeStatus string) {
+	client := twilio.NewRestClientWithParams(twilio.ClientParams{
+		Username: accountSid,
+		Password: authToken,
+	})
+
+	alert := AlertMessage{
+		To:      alertReceiver,
+		From:    fromPhoneNumber,
+		Message: CodeStatus,
+	}
+
+	params := &openapi.CreateMessageParams{}
+	params.SetTo(alert.To)
+	params.SetFrom(alert.From)
+	params.SetBody(alert.Message)
+
+	_, err := client.Api.CreateMessage(params)
+	if err != nil {
+		log.Println("Error sending alert:", err)
+	} else {
+		log.Println("Alert sent successfully")
+	}
+}
